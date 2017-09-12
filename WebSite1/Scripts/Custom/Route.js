2a27d67066e9fa6ab2460fbda3afc99e79da5816
Raja <rajabalaajar@gmail.com>
2017-09-12 12:30:01 +0530

    Changes

---
+++ WebSite1/Scripts/Custom/Route.js
@@ -0,0 +1,16 @@
+﻿var myApp = angular.module('AgrowApp', ["ngRoute"]);
+myApp.config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "Index.htm"
+    })
+    .when("/Gallery", {
+        templateUrl: "Views/Gallery.htm"
+    })
+    .when("/About", {
+        templateUrl: "Views/About.htm"
+    })
+    .when("/Contact", {
+        templateUrl: "Contact.htm"
+    });
+});
